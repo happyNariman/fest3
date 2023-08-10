9b0d008f65b9202ac4da307abf852c7b49af55c2
Maushish Yadav <chamarnumber102@gmail.com>
2023-08-11 04:48:16 +0530

    Added metamask wallet

--- fest3.frontend/src/features/login/Login.tsx
+++ fest3.frontend/src/features/login/Login.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import { RightCircleOutlined } from '@ant-design/icons'
 
 import './Login.scss'
 import logoWorldId from '@assets/logo-world-id.svg'
 import logoMetamask from '@assets/logo-metamask.svg'
 import logoTrustWallet from '@assets/logo-trustwallet.svg'
+function MetamaskLogin() { 
+  const connectMetamask = async () => {
+    if(typeof window.ethereum !== 'undefined' && window.ethereum.isMetaMask!="undefined"){
+      try{
+        /*Metamask is installed*/
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        console.log(accounts[0]);
+
+      }catch(err){
+        console.error(err.message);
+      };
+    }
+    else{
+      /*Metamask is not installed*/
+      console.log("Please install Metamask");
+  }
+}
 
 
-export default function Login() {
+
+      
+  
+export default function () {
+
   return (
     <div className='login'>
       <div className='title'>Login</div>
@@ -27,7 +48,9 @@ export default function Login() {
         <div className="wallet">
           <img className='icon' src={logoMetamask} />
           <div className='name'>
-            <div>Metamask</div>
+            <div>
+              <button onClick={connectMetamask}>Metamask</button>//style this one
+            </div>
             <div className='description'>Get +1 reputation point</div>
           </div>
           <RightCircleOutlined />
@@ -45,3 +68,7 @@ export default function Login() {
     </div>
   )
 }
+}
+
+
+
