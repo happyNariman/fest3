4e2c917ddb3288618635557b3d1840b6951b10f0
Nariman <nariman.mavliutov@gmail.com>
2023-08-12 02:47:35 +0700

    added event page

--- fest3.frontend/src/App.tsx
+++ fest3.frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Col, Row } from 'antd';
 
-import './styles/app.scss'
-import Layout from "@components/core/Layout"
-import Intro from '@pages/intro/Intro'
-import Events from '@pages/events/Events'
-import Login from '@features/login/Login'
-import { Col, Row } from 'antd'
+import './styles/app.scss';
+import Layout from '@components/core/Layout';
+import Intro from '@pages/intro/Intro';
+import Events from '@pages/events/Events';
+import Event from '@pages/event/Event';
+import Login from '@features/login/Login';
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
           <Route index element={<Intro />} />
           <Route path="intro" element={<Intro />} />
           <Route path="events" element={<Events />} />
-          <Route path="login" element={<Row><Col span={8} offset={8}><Login /></Col></Row>} />
+          <Route path="event" element={<Event />} />
+          <Route path="login" element={<Row justify='center'><Col span={8}><Login /></Col></Row>} />
         </Route>
       </Routes>
     </BrowserRouter>
